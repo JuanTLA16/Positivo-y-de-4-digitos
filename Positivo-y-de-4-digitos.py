4cd9f0790a86d8c18362729f88792f5a2dee8bf5
JuanTLA16 <125713407+JuanTLA16@users.noreply.github.com>
2023-03-06 17:02:25 -0500

    Add files via upload

---
+++ Positivo-y-de-4-digitos.py
@@ -0,0 +1,23 @@
+
+
+print("-------------------------------------------")
+print("-----------POSITIVO Y DE 4 DIGITOS---------")
+print("-------------------------------------------")
+
+
+x = int(input(" Digite un valor: "))
+
+
+if(x>0):
+    if(x>999 and x<10000):
+        print(" Positivo y de 4 digitos.")
+    else:
+        print(" Positivo pero no de 4 digitos.")
+else:
+    print(" No es positivo. ")
+
+
+
+print("-------------------------------------------")
+print("-----------------RESULTADOS----------------")
+print("-------------------------------------------")
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Positivo-y-de-4-digitos
 Verifica si un numero es positivo y de 4 digitos.
+
+# Analisis
+
+# Diseño
+
+## Diagrama de flujo
+![Diagrama de flujo](diagrama.png " Diagrama de flujo ")
